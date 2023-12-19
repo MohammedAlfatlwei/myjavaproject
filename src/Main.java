bdc11cd810e66a5f82a39c817f3d251f07860a8a
mohammed alfatlawie <mohya992@gmail.com>
2023-12-19 19:01:52 +0300

    im updating the project again

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-     final float pi= 3.14F;
+     final float pi = 3.14F;
         System.out.println(pi);
 
     }
