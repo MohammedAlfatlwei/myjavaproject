2696a49ffcf614fadfe558d4e5eab0088f8130f8
mohammed alfatlawie <mohya992@gmail.com>
2023-12-19 19:01:01 +0300

    im updating the project again

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("me updating this project to leatn how to upload it to github");
-        System.out.println("this\ttime practsing to commit and push another modification ");
-        int[] numbers = new int[3];
-        numbers[0] = 1;
-        numbers[1] = 2;
-        numbers[2] = 3;
+     final float pi= 3.14F;
+        System.out.println(pi);
 
-        System.out.println(Arrays.toString(numbers));
     }
 }
