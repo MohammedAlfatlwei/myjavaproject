6314d14ffca9ac57654e6ff93c15e625115a0b55
mohammed alfatlawie <mohya992@gmail.com>
2023-12-19 09:49:43 +0300

    this is the first commit today at 9:44

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("me updating this project to leatn how to upload it to github");
+        System.out.println("this time practsing to commit and push another modification ");
     }
 }
