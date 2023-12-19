dfcf20d3284c528983287c247b19a626582de0f0
mohammed alfatlawie <mohya992@gmail.com>
2023-12-19 10:09:17 +0300

    im updating the project again

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("me updating this project to leatn how to upload it to github");
-        System.out.println("this time practsing to commit and push another modification ");
+        System.out.println("this\ttime practsing to commit and push another modification ");
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+
+        System.out.println(Arrays.toString(numbers));
     }
 }
