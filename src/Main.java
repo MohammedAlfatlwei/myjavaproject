7a8fe22a305739d213fad7a00e7186614ce68cf3
mohammed alfatlawie <mohya992@gmail.com>
2023-12-21 21:49:44 +0300

    im updating the project again

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
-import java.util.Arrays;
-
 public class Main {
-    public static void main(String[] args) {
+    public static <string> void main(String[] args) {
      final float pi = 3.14F;
         System.out.println(pi);
+        String clint_name = "mohammed ali mohammed hussain";
+        System.out.println(clint_name);
+        int x;
+        x = 50 ;
+        System.out.println(x);
+        System.out.println();
+        //......................................................
+        double h = 3.4 ;
+        int y = (int)h + 2 ;
+        System.out.println(y);
+        // this type called explicit casting
+        String greetingmsg = "hello world";
+        //............................................................
+        // look at this beautiful shit
+        // if i want to convert a string into any kind of data type ...
+        String my_age = "19";
+        Integer.parseInt(my_age);
+        Short.parseShort(my_age)
+        Float.parseFloat(my_age)
+        System.out.println(my_age);
+        //..................................................................
+
+
 
     }
 }
